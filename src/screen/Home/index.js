06c3460367ce715ef5bdd8a5c993491681a4ef1e
LUIZDV1D <gamesbrdavi@gmail.com>
2019-06-11 16:36:18 -0300

    consumindo API

--- src/screen/Home/index.js
+++ src/screen/Home/index.js
@@ -6,11 +6,29 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 // import { Container } from './styles';
 
 export default class Home extends Component {
+
+
+  state = {
+    data: []
+  }
+
+  async componentDidMount() {
+    await fetch('https://api.github.com/users/LUIZDV1D')
+    .then(response => {
+      return response.json() 
+    })
+    .then(data => {
+      this.setState({
+        data
+      })
+    })
+  }
+
     static navigationOptions = {
         tabBarIcon: <Icon name="home" size={30} color="#000000" />
     }
   render() {
-    return <View><Text>Home</Text></View>;
+    return <View><Text>{JSON.stringify(this.state.data)}</Text></View>;
   }
 }
 
